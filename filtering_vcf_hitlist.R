11630e59f005a471f618f5f474baaff6313f4e53
ieuangw <53905031+ieuangw@users.noreply.github.com>
2019-08-27 16:15:11 +0100

    Add files via upload

--- filtering_vcf_hitlist.R
+++ filtering_vcf_hitlist.R
@@ -11,3 +11,20 @@ setwd(wd)
 #get the files
 SGK1_whitelist_hg38 <- read_csv("SGK1_whitelist_hg38.csv")
 SGK1_hitlist <- read_csv("SGK1_mutations2.csv")
+
+# using the dplyr function
+
+sgk1hitlist.tbl<- as_tibble(SGK1_hitlist)
+
+
+xy<-sgk1hitlist.tbl %>% filter(FILTER == "PASS") %>%
+                        mutate(confidence= case_when(
+                          depthALT<8 ~"low confidence",
+                          CONSEQUENCE== "nonsynonymous" &  depthALT>8 & start%in%SGK1_whitelist_hg38$start ~"High Confidence",
+                          CONSEQUENCE== "synonymous" &depthALT>8 & start%in%SGK1_whitelist_hg38$start   ~"medium confidence" ,
+                          depthALT>8 & !start%in%SGK1_whitelist_hg38$start ~ "not in whitelist",
+                          TRUE ~"other"))
+
+xy%>% count(confidence)
+
+     
